90f2107dd34ef8ef975de732c683e413ad0ea176
alena <evstafienko_a@ukr.net>
2022-11-24 22:24:44 +0200

    1.2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
-// Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import galleryCardTpl from '../templates/gallery_cards.hbs';
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+galleryEl.addEventListener('click', onClickCreateModalWindow);
+
+const itemEl = galleryItems.map(galleryCardTpl).join('');
+galleryEl.insertAdjacentHTML('beforeend', itemEl);
+
+function onClickCreateModalWindow(e) {
+  // забороняємо стандартні дії
+  e.preventDefault();
+  // вказую, що відкривати лише img
+  const isIMGEl = e.target.nodeName;
+  if (isIMGEl !== 'IMG') {
+    return;
+  }
+}
+// підключаємо бібліотеку SimpleLightbox
+const lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionsDelay: 250,
+});
